86893839e642cd8c0885e58e12bcc847789153d4
Justin Myers <justin@colortechnology.com>
2023-01-24 06:56:35 -0800

    initial petasos commit

---
+++ petasos_cluster_manager.rb
@@ -0,0 +1,12 @@
+# ssh -t xxx.xxx.xxx.xxx "cd /directory_wanted ; bash --login"
+
+# for every node location
+
+# ssh into it and grab its import/export file
+
+# with the import/export files I have, build a hash of pools
+# with lists of places files come from and lists of places files go to
+
+# for each exporting node per pool, ssh into it and grab its export files for the pools
+# if there are export files I haven't seen before, scp all those files into each of the
+# pool import locations (very much like the location process)
---
+++ petasos_node_manager.rb
@@ -0,0 +1,29 @@
+require "./petasos_location"
+
+@node_config = {
+  name: "petasos-node-1",
+  locations: [
+    {
+      path: File.join(Dir.pwd, "location_a"),
+    },
+  ],
+}
+
+# a list of pools and their import paths from locations
+@pools = Hash.new { |h, k| h[k] = [] }
+@node_config[:locations].each do |location|
+  p = PetasosLocation.new(location[:path], @node_config[:name])
+  p.pools.each do |pool|
+    pool_import_path = p.pool_import_path(pool)
+    @pools[pool["name"]] << pool_import_path if pool_import_path
+  end
+end
+File.open(File.join("#{@node_config[:name]}_pool-import-locations.yaml"), "w") do |out|
+  YAML.dump(@pools, out)
+end
+
+@node_config[:locations].each do |location|
+  p = PetasosLocation.new(location[:path], @node_config[:name])
+  p.clear_all_seen_pool_files
+  p.run
+end
