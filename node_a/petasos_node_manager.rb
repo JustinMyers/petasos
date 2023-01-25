95121c0a949b8016944ca4415c910dee685e3bae
Justin Myers <justin@colortechnology.com>
2023-01-24 17:16:22 -0800

    put full path of imports location

--- node_a/petasos_node_manager.rb
+++ node_a/petasos_node_manager.rb
@@ -4,7 +4,7 @@ require_relative "./petasos_location"
   name: "petasos-node-a",
   locations: [
     {
-      path: File.join(File.dirname(__FILE__), "location_a"),
+      path: File.join(File.dirname(File.absolute_path(__FILE__)), "location_a"),
     },
   ],
 }
@@ -18,7 +18,7 @@ require_relative "./petasos_location"
     @pools[pool["name"]] << pool_import_path if pool_import_path
   end
 end
-File.open(File.join(File.dirname(__FILE__), "imports_#{@node_config[:name]}.yaml"), "w") do |out|
+File.open(File.join(File.dirname(File.absolute_path(__FILE__)), "imports_#{@node_config[:name]}.yaml"), "w") do |out|
   YAML.dump(@pools, out)
 end
 
