0a57cb5ea5e8d79f18b333f464463f0b2d382927
Justin Myers <justin@colortechnology.com>
2023-01-24 17:26:01 -0800

    reset and run file

---
+++ clear_and_run.sh
@@ -0,0 +1,8 @@
+rm cluster/imports_*
+rm cluster/exports_*
+rm node_a/imports_*
+rm node_a/exports_*
+rm node_a/location_a/seen_*
+rm node_b/imports_*
+rm node_b/exports_*
+rm node_b/location_a/seen_*
