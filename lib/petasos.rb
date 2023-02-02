c6b813973ac845e6de8ba1f6aaf71777016501f9
Justin Myers <justin@tenmillionyears.org>
2023-02-02 03:48:54 +0000

    bundle gem init.

---
+++ lib/petasos.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "petasos/version"
+
+module Petasos
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/petasos/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Petasos
+  VERSION = "0.1.0"
+end
