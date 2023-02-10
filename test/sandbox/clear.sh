cd2899b3d39222a8b1554595499d36dba0d0a3e7
Justin Myers <justin@colortechnology.com>
2023-02-10 12:12:44 -0800

    update clear to remove manifests.

--- test/sandbox/clear.sh
+++ test/sandbox/clear.sh
@@ -1,12 +1,12 @@
-rm distributor/imports_*
+rm distributor/manifest_*
 rm distributor/exports_*
 rm distributor/completed-*
 rm -rf distributor/petasos-node-*
-rm node_a/imports_*
+rm node_a/manifest_*
 rm node_a/exports_*
 rm node_a/completed-*
 rm node_a/seen_*
-rm node_b/imports_*
+rm node_b/manifest_*
 rm node_b/exports_*
 rm node_b/seen_*
 rm node_b/location_a/data/*
